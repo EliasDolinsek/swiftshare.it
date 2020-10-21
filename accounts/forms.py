40aac0e158c94a38b93bfd84f5803b8154a57811
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-10-21 16:41:04 +0200

    Updated user creation and user change forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,10 +6,10 @@ from accounts.models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('email', 'first_name', 'last_name')
+        fields = ('first_name', 'last_name', 'email')
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('email', 'first_name', 'last_name')
+        fields = ('first_name', 'last_name', "email")
